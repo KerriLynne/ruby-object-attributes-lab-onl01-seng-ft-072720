17b336bb6d20d80bb0aaa58da4dc9ed05f59a965
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-02 19:13:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(fido)
+    this_dogs_name = fido
   end
 
   def name
     this_dogs_name
   end
 end
-
-fido = Dog.new
-fido.name = "Fido"
